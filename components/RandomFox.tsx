66534a0cb47827c1ca1647ddae3622e52f19e997
Jose Villeda <jvilleda-2021075@kinal.edu.gt>
2023-08-15 12:24:03 -0600

    Agregando Observadores para las lazy images

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,12 +1,33 @@
-import React from "react";
+import { useRef, useEffect, useState } from "react";
+import App from "../pages/_app";
 
-//Funcion para generar numeros del 1 al 123
-const generateRandomNumber = (): number => Math.floor(Math.random() * 123) + 1;
+type Props = { image: string };
 
-console.log(generateRandomNumber)   
+export const RandomFox = ({ image }: Props): JSX.Element => {
+  const node = useRef<HTMLImageElement>(null);
 
-export const RandomFox = (): JSX.Element => {
-  const image: string = `https://randomfox.ca/images/${generateRandomNumber()}.jpg`;
+  const [src, setSrc] = useState<string>(
+    "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+  );
 
-  return <img width={320} height="auto" className="rounded" src={image} />;
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setSrc(image);
+        }
+      });
+    });
+    if (node.current) {
+      observer.observe(node.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [image]);
+
+  return (
+    <img ref={node} width={320} height="auto" className="rounded bg-gray-300" src={src} />
+  );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,43 @@
 import Image from "next/image";
+import type { MouseEventHandler } from "react";
+import { useState } from "react";
 import { Inter } from "next/font/google";
 import { RandomFox } from "@/components/RandomFox";
+import { log } from "console";
 
 const inter = Inter({ subsets: ["latin"] });
 
+type ImageItem = { id: string; url: string };
+
+const generateRandomNumber = (): number => Math.floor(Math.random() * 123) + 1;
+
+const generateId = (): string =>
+  Math.random().toString(36).substring(2) + Date.now().toString(36);
 
 export default function Home() {
+  //Otra Opcion es  = useState<Array<string>>
+  const [images, setImages] = useState<Array<ImageItem>>([]);
+
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
+
+    const newImageItem = {
+      id: generateId(),
+      url: `https://randomfox.ca/images/${generateRandomNumber()}.jpg`,
+    };
+
+    setImages([...images, newImageItem]);
+  };
+
   return (
     <main>
       <h1 className="text-3xl font-bold">Hola Guate!</h1>
-      <RandomFox />
+      <button onClick={addNewFox}> Add a new Fox </button>
+      {images.map((i) => (
+        <div key={i.id} className="p-4">
+          <RandomFox image={i.url} />
+        </div>
+      ))}
     </main>
   );
 }
+
